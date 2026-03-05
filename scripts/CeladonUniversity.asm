6ba31ccda500acd4bb2c4bc26a22eaf4dbc1bd2c
TotalTS <triumphatussad@gmail.com>
2026-03-05 01:51:10 -0300

    fixed CeladonUniversity script, added palettes

--- scripts/CeladonUniversity.asm
+++ scripts/CeladonUniversity.asm
@@ -31,24 +31,19 @@ CeladonUniversitySignText3:
 CeladonUniversitySignText4:
 	text_asm
 	CheckEvent EVENT_GOT_UNIVERSITY_MAGIKARP
-    jp nz, .diplomaWon
-	
+	jp nz, .diplomaWon
 	ld hl, .CeladonUniversityDiploma1
 	call PrintText
-	
 	jp TextScriptEnd
 	
 .diplomaWon
 	ld hl, .CeladonUniversityDiploma2
 	call PrintText
-	
 	ld a, 1 ; Diploma check
 	ld [wWhichTrade], a
-	
 	callfar DisplayDiploma
 	ld a, TRUE
 	ld [wDoNotWaitForButtonPressAfterDisplayingText], a
-	
 	xor a
 	ld [wWhichTrade], a
 	jp TextScriptEnd
@@ -76,194 +71,193 @@ CeladonUniversityBlackboard:
 CeladonUniversityProfText:
 	text_asm
 	CheckEvent EVENT_GOT_UNIVERSITY_MAGIKARP
-    jp nz, .alreadyWon
-    ld hl, .IntroText
-    call PrintText
-
-    call YesNoChoice
-    ld a, [wCurrentMenuItem]
-    and a
-    jp nz, .refusedExam
-	
+	jp nz, .alreadyWon
+	ld hl, .IntroText
+	call PrintText
+	call YesNoChoice
+	ld a, [wCurrentMenuItem]
+	and a
+	jp nz, .refusedExam
 	call CeladonUniversityHyperExam
-    jp c, .failedExam
-	
-    ld a, [wPartyCount]
-    cp 6
-    jr nz, .giveMagikarp
-    ld hl, .PartyFullText
-    call PrintText
-    jp .done
-
+	jp c, .failedExam
+	ld a, [wPartyCount]
+	cp 6
+	jr nz, .giveMagikarp
+	ld hl, .PartyFullText
+	call PrintText
+	jp .done
 .giveMagikarp
-    ld hl, .CeladonUniversityProfPassText
-    call PrintText
-
-    ; --- PREPARE DATA ---
-    ld a, MAGIKARP
-    ld [wInGameTradeReceiveMonSpecies], a
-    ld [wCurPartySpecies], a
-    ld a, 5
-    ld [wCurEnemyLevel], a
-
-    ; Prepare OT ID and Name (Based on the player)
+	ld hl, .CeladonUniversityProfPassText
+	call PrintText
+	; prepare data
+	ld a, MAGIKARP
+	ld [wInGameTradeReceiveMonSpecies], a
+	ld [wCurPartySpecies], a
+	ld a, 5
+	ld [wCurEnemyLevel], a
+	; Prepare OT ID and Name (Based on the player)
 	ld a, [wPartyCount]
-    dec a
-    ld hl, wPartyMonOT
-    ld bc, NAME_LENGTH
-    call AddNTimes
-    ld d, h
-    ld e, l
-    ld hl, wPlayerName ; Copy player name as Original Trainer
-    ld bc, NAME_LENGTH
-    call CopyData
-	
-    ld hl, wPlayerName
-    ld de, wTradedEnemyMonOT
+	dec a
+	ld hl, wPartyMonOT
 	ld bc, NAME_LENGTH
-    call CopyData
-
-    ld hl, wPlayerID
-    ld de, wTradedEnemyMonOTID
-    ld bc, 2
-    call CopyData
-	
+	call AddNTimes
+	ld d, h
+	ld e, l
+	ld hl, wPlayerName ; Copy player name as Original Trainer
+	ld bc, NAME_LENGTH
+	call CopyData
+	ld hl, wPlayerName
+	ld de, wTradedEnemyMonOT
+	ld bc, NAME_LENGTH
+	call CopyData
+	ld hl, wPlayerID
+	ld de, wTradedEnemyMonOTID
+	ld bc, 2
+	call CopyData
 	ld hl, .CustomOTName
-    ld de, wLinkEnemyTrainerName
+	ld de, wLinkEnemyTrainerName
 	ld bc, NAME_LENGTH
-    call CopyData
-	
-    ld a, 1
-    ldh [hSpriteIndex], a
-    ld a, SPRITE_FACING_UP
-    ldh [hSpriteFacingDirection], a
-    call SetSpriteFacingDirectionAndDelay
-
-    ld hl, .CeladonUniversityProfPassText2
-    call PrintText
-	
+	call CopyData
+	ld a, 1
+	ldh [hSpriteIndex], a
+	ld a, SPRITE_FACING_UP
+	ldh [hSpriteFacingDirection], a
+	call SetSpriteFacingDirectionAndDelay
+	ld hl, .CeladonUniversityProfPassText2
+	call PrintText
 	ld a, SFX_TELEPORT_EXIT_1
-    call PlaySound
-    call WaitForSoundToFinish
-
-    ; --- ANIMATION AND ADDITION ---
-    call LoadHpBarAndStatusTilePatterns
-    predef UniversityDistroAnim
-
-    ; Define origins of data for AddPartyMon
+	call PlaySound
+	call WaitForSoundToFinish
+	; trade animation
+	call LoadHpBarAndStatusTilePatterns
+	predef UniversityDistroAnim
+	; Define origins of data for AddPartyMon
 	ld hl, .ReceivedMagikarpText
-    call PrintText
-    ld a, SFX_GET_KEY_ITEM ; Music
-    call PlaySound
-    call WaitForSoundToFinish
-    xor a
-    ld [wMonDataLocation], a
-    call AddPartyMon
-	
+	call PrintText
+	ld a, SFX_GET_KEY_ITEM ; Music
+	call PlaySound
+	call WaitForSoundToFinish
+	xor a
+	ld [wMonDataLocation], a
+	call AddPartyMon
 	ld a, [wPartyCount]
-    dec a
-    ld hl, wPartyMon1Moves
-    ld bc, wPartyMon2 - wPartyMon1
-    call AddNTimes ; HL now points the moves of te recently acquired Magikarp
-
-    ; Define moves
-    ld [hl], SPLASH        ; First move
-    inc hl
-    ld [hl], DRAGON_RAGE   ; Second move
-    inc hl
-    ld [hl], 0             ; Empty
-    inc hl
-    ld [hl], 0             ; Empty
-
-    ; Define PP
-    push hl
-    ld bc, wPartyMon1PP - (wPartyMon1Moves + 4)
-    add hl, bc
-    inc hl
-    ld [hl], 40            ; Splash PP
-    inc hl
-    ld [hl], 10            ; Dragon Rage PP
-    pop hl
-
+	dec a
+	ld hl, wPartyMon1Moves
+	ld bc, wPartyMon2 - wPartyMon1
+	call AddNTimes ; HL now points the moves of te recently acquired Magikarp
+	; Define moves
+	ld [hl], SPLASH ; First move
+	inc hl
+	ld [hl], DRAGON_RAGE ; Second move
+	inc hl
+	ld [hl], 0 ; Empty
+	inc hl
+	ld [hl], 0 ; Empty
+	; Define PP
+	push hl
+	ld bc, wPartyMon1PP - (wPartyMon1Moves + 4)
+	add hl, bc
+	inc hl
+	ld [hl], 40 ; Splash PP
+	inc hl
+	ld [hl], 10 ; Dragon Rage PP
+	pop hl
 	SetEvent EVENT_GOT_UNIVERSITY_MAGIKARP
-    ; --- CLEANING ---
+	; cleaning screen
 	ld a, $90
-    ldh [hWY], a
-	
-    call ClearScreen
-    farcall InGameTrade_RestoreScreen
-	
+	ldh [hWY], a
+	call ClearScreen
+	farcall InGameTrade_RestoreScreen
 	farcall RedrawMapView
 	call UpdateSprites
-	
 	call DelayFrame
 	call DelayFrame
 	xor a
-    ldh [hWY], a      ; re-enable window for textbox
-	
+	ldh [hWY], a	  ; re-enable window for textbox
+	; make look at player again
+	ld a, [wSpritePlayerStateData1FacingDirection]
+	cp SPRITE_FACING_DOWN
+	jr z, .skip_change
+	cp SPRITE_FACING_UP
+	jr z, .prof_face_down
+	cp SPRITE_FACING_LEFT
+	jr z, .prof_face_right
+	; else assume facing right
+.prof_face_left
+	ld a, SPRITE_FACING_LEFT
+	jr .set_prof_facing
+.prof_face_down
+	ld a, SPRITE_FACING_DOWN
+	jr .set_prof_facing
+.prof_face_right
+	ld a, SPRITE_FACING_RIGHT
+.set_prof_facing
+	ldh [hSpriteFacingDirection], a
+	ld a, 1
+	ldh [hSpriteIndex], a
+	call SetSpriteFacingDirectionAndDelay
+.skip_change
 	ld hl, .AfterGiftText
-    call PrintText
-
-    jp TextScriptEnd
-
+	call PrintText
+	jp TextScriptEnd
 .done
-    jp TextScriptEnd
+	jp TextScriptEnd
 
 .CeladonUniversityProfPassText:
-    text_far _CeladonUniversityProfPassText
-    text_end
+	text_far _CeladonUniversityProfPassText
+	text_end
 	
 .CeladonUniversityProfPassText2:
-    text_far _CeladonUniversityProfPassText2
-    text_end
+	text_far _CeladonUniversityProfPassText2
+	text_end
 
 .ReceivedMagikarpText:
-    text "Received a"
+	text "Received a"
 	line "MAGIKARP with"
 	cont "DRAGON RAGE!@"
-    text_end
+	text_end
 	
 .CustomOTName:
-    db "UNIVERSITY@"
+	db "UNIVERSITY@"
 	
 .PartyFullText:
 	text_far _PartyFullText
 	text_end
 	
 .alreadyWon
-    ld hl, .AlreadyWonText
-    call PrintText
-    jp .done
+	ld hl, .AlreadyWonText
+	call PrintText
+	jp .done
 
 .refusedExam
-    ld hl, .RefusedText
-    call PrintText
-    jp .done
+	ld hl, .RefusedText
+	call PrintText
+	jp .done
 
 .failedExam
-    ld hl, .FailedText
-    call PrintText
-    jp .done
+	ld hl, .FailedText
+	call PrintText
+	jp .done
 
 .IntroText:
-    text_far _CeladonUniversityProfText
-    text_end
+	text_far _CeladonUniversityProfText
+	text_end
 
 .RefusedText:
-    text_far _CeladonUniversityProfNoText
-    text_end
+	text_far _CeladonUniversityProfNoText
+	text_end
 
 .FailedText:
-    text_far _CeladonUniversityProfFailText
-    text_end
+	text_far _CeladonUniversityProfFailText
+	text_end
 
 .AlreadyWonText:
-    text_far _CeladonUniversityProfAlreadyWonText
-    text_end
+	text_far _CeladonUniversityProfAlreadyWonText
+	text_end
 
 .AfterGiftText:
-    text_far _CeladonUniversityProfAfterGiftText
-    text_end
+	text_far _CeladonUniversityProfAfterGiftText
+	text_end
 
 
 CeladonUniversityReceText:
@@ -280,60 +274,53 @@ CeladonUniversityCTFText:
 
 CeladonUniversityHyperExam:
 	ld hl, CeladonUniversityProfStartExamText
-    call PrintText	
-	
-    call EnableAutoTextBoxDrawing
-
-; ========== Question 1 ==========
-    ld hl, CeladonUniversityProfQ1Text
-    call PrintText
-    call YesNoChoice
-    ld a, [wCurrentMenuItem]
-    and a
-    jr nz, .fail
-
-    ld hl, CeladonUniversityProfCorrectText
-    call PrintText
-
-; ========== Question 2 ==========
-    ld hl, CeladonUniversityProfQ2Text
-    call PrintText
-    call YesNoChoice
-    ld a, [wCurrentMenuItem]
-    and a
-    jr z, .fail
-
-    ld hl, CeladonUniversityProfCorrectText
-    call PrintText
-
+	call PrintText
+	call EnableAutoTextBoxDrawing
+; Question 1
+	ld hl, CeladonUniversityProfQ1Text
+	call PrintText
+	call YesNoChoice
+	ld a, [wCurrentMenuItem]
+	and a
+	jr nz, .fail
+	ld hl, CeladonUniversityProfCorrectText
+	call PrintText
+; Question 2
+	ld hl, CeladonUniversityProfQ2Text
+	call PrintText
+	call YesNoChoice
+	ld a, [wCurrentMenuItem]
+	and a
+	jr z, .fail
+	ld hl, CeladonUniversityProfCorrectText
+	call PrintText
 ; success
-    and a
-    ret
-
+	and a
+	ret
 .fail
-    scf
-    ret
+	scf
+	ret
 
 CeladonUniversityProfStartExamText:
-    text_far _CeladonUniversityProfStartExamText
-    text_end
+	text_far _CeladonUniversityProfStartExamText
+	text_end
 
 CeladonUniversityProfPassedText:
-    text_far _CeladonUniversityProfPassText
-    text_end
+	text_far _CeladonUniversityProfPassText
+	text_end
 
 CeladonUniversityProfQ1Text:
-    text_far _CeladonUniversityProfQ1Text
-    text_end
+	text_far _CeladonUniversityProfQ1Text
+	text_end
 
 CeladonUniversityProfQ2Text:
-    text_far _CeladonUniversityProfQ2Text
-    text_end
+	text_far _CeladonUniversityProfQ2Text
+	text_end
 
 CeladonUniversityProfCorrectText:
-    text_far _CeladonUniversityProfCorrectText
-    text_end
+	text_far _CeladonUniversityProfCorrectText
+	text_end
 
 CeladonUniversityProfFailText:
-    text_far _CeladonUniversityProfFailText
-    text_end
+	text_far _CeladonUniversityProfFailText
+	text_end
