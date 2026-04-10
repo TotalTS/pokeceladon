b82022e5b8b9b92e596cda1a34a368a0563da78a
TotalTS <triumphatussad@gmail.com>
2026-04-10 19:17:03 -0300

    Fixed Mystery Cave script

--- scripts/MysteryCave.asm
+++ scripts/MysteryCave.asm
@@ -6,5 +6,89 @@ MysteryCave_TextPointers:
 	dw_const MysteryCaveSuperNerdText, TEXT_MYSTERYCAVE_SUPER_NERD
 	
 MysteryCaveSuperNerdText:
-	text_far _MysteryCaveSuperNerdText
-	text_end
+    text_asm
+    CheckEvent EVENT_MYSTERYCAVE_GOT_HELIX_FOSSIL
+    jr nz, .alreadyGot
+    CheckEvent EVENT_MYSTERYCAVE_GOT_DOME_FOSSIL
+    jr nz, .alreadyGot
+
+    CheckEvent EVENT_GOT_HELIX_FOSSIL
+    jr nz, .OfferDome
+    CheckEvent EVENT_GOT_DOME_FOSSIL
+    jr nz, .OfferHelix
+    jr .alreadyGot
+
+.OfferDome
+    ld hl, .MysteryCaveSuperNerdText
+    call PrintText
+    ld a, $1
+    ld [wDoNotWaitForButtonPressAfterDisplayingText], a
+    ld hl, .YouWantDomeText
+    call PrintText
+    call YesNoChoice
+    ld a, [wCurrentMenuItem]
+    and a
+    jr nz, .done
+    lb bc, DOME_FOSSIL, 1
+    call GiveItem
+    jp nc, .bag_full
+    ld hl, .MysteryCaveSuperNerdReceivedFossilText
+    call PrintText
+    SetEvent EVENT_MYSTERYCAVE_GOT_DOME_FOSSIL
+    jr .done
+
+.OfferHelix
+    ld hl, .MysteryCaveSuperNerdText
+    call PrintText
+    ld a, $1
+    ld [wDoNotWaitForButtonPressAfterDisplayingText], a
+    ld hl, .YouWantHelixText
+    call PrintText
+    call YesNoChoice
+    ld a, [wCurrentMenuItem]
+    and a
+    jr nz, .done
+    lb bc, HELIX_FOSSIL, 1
+    call GiveItem
+    jp nc, .bag_full
+    ld hl, .MysteryCaveSuperNerdReceivedFossilText
+    call PrintText
+    SetEvent EVENT_MYSTERYCAVE_GOT_HELIX_FOSSIL
+    jr .done
+
+.alreadyGot
+    ld hl, .AlreadyGotText
+    call PrintText
+.done
+    jp TextScriptEnd
+
+.bag_full
+    ld hl, .MysteryCaveSuperNerdYouHaveNoRoomText
+    call PrintText
+    jp TextScriptEnd
+	
+.MysteryCaveSuperNerdText
+    text_far _MysteryCaveSuperNerdText
+    text_end
+
+.AlreadyGotText
+    text_far _MysteryCaveSuperNerdAlreadyGotText
+    text_end
+	
+.MysteryCaveSuperNerdYouHaveNoRoomText
+    text_far _MysteryCaveSuperNerdYouHaveNoRoomText
+    text_end
+
+.YouWantDomeText
+    text_far _MtMoonB2FDomeFossilYouWantText
+    text_end
+
+.YouWantHelixText
+    text_far _MtMoonB2FHelixFossilYouWantText
+    text_end
+
+.MysteryCaveSuperNerdReceivedFossilText
+    text_far _MysteryCaveSuperNerdReceivedFossilText
+    sound_get_item_1
+    text_promptbutton
+    text_end
--- text/MysteryCave.asm
+++ text/MysteryCave.asm
@@ -1,70 +1,4 @@
 _MysteryCaveSuperNerdText::
-	text_asm
-	CheckEvent EVENT_MYSTERYCAVE_GOT_HELIX_FOSSIL
-	jr nz, .alreadyGot
-	CheckEvent EVENT_MYSTERYCAVE_GOT_DOME_FOSSIL
-	jr nz, .alreadyGot
-
-	CheckEvent EVENT_GOT_HELIX_FOSSIL
-	jr nz, .OfferDome
-	CheckEvent EVENT_GOT_DOME_FOSSIL
-	jr nz, .OfferHelix
-	jr .alreadyGot
-
-.OfferDome
-	ld hl, .MysteryCaveSuperNerdText
-	call PrintText
-	ld a, $1
-	ld [wDoNotWaitForButtonPressAfterDisplayingText], a
-	ld hl, .YouWantDomeText
-	call PrintText
-	call YesNoChoice
-	ld a, [wCurrentMenuItem]
-	and a
-	jr nz, .done
-	lb bc, DOME_FOSSIL, 1
-	call GiveItem
-	jp nc, .bag_full
-	ld hl, .MysteryCaveSuperNerdReceivedFossilText
-	call PrintText
-	SetEvent EVENT_MYSTERYCAVE_GOT_DOME_FOSSIL
-	jr .done
-.OfferHelix
-	ld hl, .MysteryCaveSuperNerdText
-	call PrintText
-	ld a, $1
-	ld [wDoNotWaitForButtonPressAfterDisplayingText], a
-	ld hl, .YouWantHelixText
-	call PrintText
-	call YesNoChoice
-	ld a, [wCurrentMenuItem]
-	and a
-	jr nz, .done
-	lb bc, HELIX_FOSSIL, 1
-	call GiveItem
-	jp nc, .bag_full
-	ld hl, .MysteryCaveSuperNerdReceivedFossilText
-	call PrintText
-	SetEvent EVENT_MYSTERYCAVE_GOT_HELIX_FOSSIL
-	jr .done
-.alreadyGot
-	ld hl, .AlreadyGotText
-	call PrintText
-.done
-	jp TextScriptEnd
-
-.bag_full
-	ld hl, .MysteryCaveSuperNerdYouHaveNoRoomText
-	call PrintText
-	jp TextScriptEnd
-
-.MysteryCaveSuperNerdReceivedFossilText
-	text_far _MysteryCaveSuperNerdReceivedFossilText
-	sound_get_item_1
-	text_promptbutton
-	text_end
-
-.MysteryCaveSuperNerdText
 	text "Oh! You're the"
 	line "CHAMPION!"
 	
@@ -77,15 +11,7 @@ _MysteryCaveSuperNerdText::
 	line "FOSSIL!"
 	prompt
 
-.YouWantDomeText
-	text_far _MtMoonB2FDomeFossilYouWantText
-	text_end
-
-.YouWantHelixText
-	text_far _MtMoonB2FHelixFossilYouWantText
-	text_end
-
-.AlreadyGotText:
+_MysteryCaveSuperNerdAlreadyGotText::
 	text "This place is"
 	line "huge!"
 	
@@ -96,7 +22,7 @@ _MysteryCaveSuperNerdText::
 	line "#MON too!"
 	done
 
-.MysteryCaveSuperNerdYouHaveNoRoomText:
+_MysteryCaveSuperNerdYouHaveNoRoomText::
 	text "Look, you've got"
 	line "no room for this.@"
 	done
